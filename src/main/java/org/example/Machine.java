78059062f0be7c11e89be01377157139827b2303
jakon <jakondakandy@yandex.ru>
2025-01-07 01:29:17 +0300

    Домашняя работа

--- src/main/java/org/example/Machine.java
+++ src/main/java/org/example/Machine.java
@@ -2,26 +2,30 @@ package org.example;
 
 import java.util.Scanner;
 
-public class Machine {
 
+public class Machine {
 
     static String machineName() {                       //Метод для чтения данных о названии машин от пользователя
         Scanner scanner = new Scanner(System.in);       //Создаю сканнер
-        String machine = scanner.next();                //Принимаю значение, записываю в переменную типа строки
-        return machine;                                 //Возвращаю значение
+        //Принимаю значение, записываю в переменную типа строки
+        return scanner.next();                                 //Возвращаю значение
     }
 
-    static long speedRange() {                                  //Метод для рамок скорости
-        Scanner scanner = new Scanner(System.in);               //Создаю сканнер
+    static Integer speedRange() {                                  //Метод для рамок скорости
+                    //Создаю сканнер
         while (true) {                                          //Пишу бесконечный цикл (для случая, когда люая введенная скорость не входит в рамки
-            long speed = scanner.nextLong();
-            if (0 < speed && speed < 251) {                    //Пишу условие для составления рамок
-                return speed;                                   //Возвращаю значение
-            } else {
-                System.out.println("Неправильная скорость");    //Вывожу сообщение о неправильном вводе, если скорость не входит в рамки
+            Scanner speed = new Scanner (System.in) ;
+            if (speed.hasNextInt()) {
+                int  speedInt = speed.nextInt();
+                if (0 < speedInt && speedInt < 251) {                    //Пишу условие для составления рамок
+                    return speedInt ;                                   //Возвращаю значение
+            }   else  {
+                    System.out.println("Неправильная скорость");    //Вывожу сообщение о неправильном вводе, если скорость не входит в рамки
+            }
+        } else {
+                System.out.println("Неправильная скорость");
             }
         }
-
     }
 
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Введите Название машины №1:");
         String firstMachine = Machine.machineName();
         System.out.println("Введите скорость машины №1:");
@@ -17,13 +18,6 @@ public class Main {
 
         System.out.println(Rally.winner(firstSpeed, secondSpeed, thirdSpeed, firstMachine, secondMachine, thirdMachine));
 
-
-
-
-
     }
-
-
-
-    }
+}
 
